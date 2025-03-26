103ba2a30d7b1b965aec87cfccc23c00d36d5477
Dmitriy Nadelyaev <kopstant99@yandex.ru>
2025-03-26 16:53:06 +0300

    The admin panel is configured for easy control

--- habit/admin.py
+++ habit/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from users.models import CustomUser
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'username', 'telegram_chat_id', 'is_staff')
+    list_filter = ('is_staff', 'is_superuser', 'is_active')
+    search_fields = ('email', 'username', 'telegram_chat_id')
+    ordering = ('email',)
