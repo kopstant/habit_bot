0a976613811212dc34677ebe4f037f45fb7f0046
Dmitriy Nadelyaev <kopstant99@yandex.ru>
2025-03-26 17:07:58 +0300

    The initial model for the habit has been implemented

--- habit/models.py
+++ habit/models.py
@@ -1,3 +1,70 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+User = get_user_model()
+
+
+class Habit(models.Model):
+    PERIODICITY_CHOICES = [
+        (1, 'Ежедневно'),
+        (2, 'Каждые 2 дня'),
+        (3, 'Каждые 3 дня'),
+        (4, 'Каждые 4 дня'),
+        (5, 'Каждые 5 дней'),
+        (6, 'Каждые 6 дней'),
+        (7, 'Еженедельно'),
+    ]
+
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='habits',
+        verbose_name='Пользователь'
+    )
+    place = models.CharField(max_length=255, verbose_name='Место выполнения')
+    time = models.TimeField(verbose_name='Время выполнения')
+    action = models.CharField(max_length=255, verbose_name='Действие')
+    is_pleasant = models.BooleanField(
+        default=False,
+        verbose_name='Признак приятной привычки'
+    )
+    related_habit = models.ForeignKey(
+        'self',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+        verbose_name='Связанная привычка'
+    )
+    periodicity = models.PositiveSmallIntegerField(
+        choices=PERIODICITY_CHOICES,
+        default=1,
+        validators=[MinValueValidator(1), MaxValueValidator(7)],
+        verbose_name='Периодичность'
+    )
+    reward = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True,
+        verbose_name='Вознаграждение'
+    )
+    duration = models.PositiveSmallIntegerField(
+        validators=[MaxValueValidator(120)],
+        verbose_name='Время на выполнение (в секундах)'
+    )
+    is_public = models.BooleanField(
+        default=False,
+        verbose_name='Признак публичности'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата создания'
+    )
+
+    def __str__(self):
+        return f'Я буду {self.action} в {self.time} в {self.place}'
+
+    class Meta:
+        verbose_name = 'Привычка'
+        verbose_name_plural = 'Привычки'
+        ordering = ['-created_at']
