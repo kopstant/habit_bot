97c4f68e866c37b3d76bc99f9521f2d7ea64c5ae
Dmitriy Nadelyaev <kopstant99@yandex.ru>
2025-03-26 17:49:57 +0300

    Implemented viewsets for the habit model

--- habit/views.py
+++ habit/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from habit.paginators import CustomPaginator
+from habit.models import Habit
+from habit.serializers import HabitSerializer
+from rest_framework import viewsets
 
-# Create your views here.
+
+class HabitViewSet(viewsets.ModelViewSet):
+    serializer_class = HabitSerializer
+    pagination_class = CustomPaginator
+
+    def get_queryset(self):
+        """
+        Выполняется проверка параметров запросов и если передан параметр public(True),
+        то возвращаются только те привычки которые имеют флаг is_public=True.
+        Если параметр public не передается, то возвращаются привычки, связанные с авторизованным пользователем.
+        Каждый пользователь видит только свои привычки.
+        """
+        if self.action == 'list' and self.request.query_params.get('public'):
+            return Habit.objects.filter(is_public=True)
+        return Habit.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        """
+        При создании привычки, привязываем к ней текущего пользователя.
+        """
+        serializer.save(user=self.request.user)
