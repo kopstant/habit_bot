da786ec4e6c284a58480285eaa5e3dad11172f97
Dmitriy Nadelyaev <kopstant99@yandex.ru>
2025-03-26 16:19:18 +0300

    Views for the User model are implemented

--- users/views.py
+++ users/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets, status, permissions
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from tutorial.quickstart.serializers import UserSerializer
+from rest_framework import generics
 
-# Create your views here.
+from users.serializers import RegisterSerializer
+
+User = get_user_model()  # Получаем модель пользователя
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]  # Авторизованный пользоватеть
+
+
+class RegisterView(generics.CreateAPIView):  # Позволяет создавать нового пользователя (зарегистрировать) POST
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer  # хэширование пароля при помощи RegisterSerializer
+    permission_classes = [permissions.AllowAny]  # Доступ без авторизации. Для регистрации.
